9c8f0ca5602fcc8b3f5d98d178b22d78c8b2b729
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2023-05-29 17:04:33 -0400

    -

--- run.sh
+++ run.sh
@@ -8,9 +8,27 @@ export MILABENCH_CONFIG=${WORKDIR}/milabench/config/standard.yaml
 export MILABENCH_GPU_ARCH=rocm
 export MILABENCH_DASH=no
 
-pip install -e ${WORKDIR}/milabench
-pip install -e ${WORKDIR}/voir
 
-milabench install
-milabench prepare
-milabench run
+if [ ! -d "${WORKDIR}/base" ]; then 
+    module load python/3.9
+    python -m venv base
+
+    source base/bin/activate
+    
+    pip install --upgrade pip
+    pip install -e ${WORKDIR}/milabench
+    pip install -e ${WORKDIR}/voir
+
+    milabench install
+    milabench prepare
+fi
+
+source base/bin/activate
+
+# pip install -e ${WORKDIR}/milabench
+# pip install -e ${WORKDIR}/voir
+
+# milabench install
+# milabench prepare
+ROCR_VISIBLE_DEVICES=0,1,2,3 milabench run --select dlrm
+
